8b436c5be62800c95316cb0a7dcc3f6fa513fca3
Roman <rbster@yandex.ru>
2022-11-18 01:27:55 +0300

    hello world with colored logs

---
+++ src/main/kotlin/ru/darts/storyline/conroller/HelloWorldController.kt
@@ -0,0 +1,16 @@
+package ru.darts.storyline.conroller
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloWorldController {
+    private val log = LoggerFactory.getLogger(HelloWorldController::class.java)
+
+    @GetMapping("/hello")
+    fun hello() : String {
+        log.warn("get yo there!")
+        return "hella hi!"
+    }
+}
