a56bc726e4f93c0fbc9458a064b585c20c5e1e8f
Roman <rbster@yandex.ru>
2023-05-27 15:42:09 +0300

    fixed adapter

--- storyline/src/main/kotlin/ru/darts/storyline/conroller/HelloWorldController.kt
+++ storyline/src/main/kotlin/ru/darts/storyline/conroller/HelloWorldController.kt
@@ -1,11 +1,16 @@
 package ru.darts.storyline.conroller
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import ru.darts.storyline.integration.DbAdapter
 
 @RestController
 class HelloWorldController {
+    @Autowired
+    lateinit var dbAdapter: DbAdapter
+
     private val log = LoggerFactory.getLogger(HelloWorldController::class.java)
 
     @GetMapping("/hello")
--- storyline/src/main/kotlin/ru/darts/storyline/integration/DbAdapter.kt
+++ storyline/src/main/kotlin/ru/darts/storyline/integration/DbAdapter.kt
@@ -3,43 +3,81 @@ package ru.darts.storyline.integration
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
+import ru.darts.storyline.UniversalTimelineRq
 
 
 @Service
-class DbAdapter(private val dbClient: RestTemplate) {
-    @Value("\${dbClient.url:http://127.0.0.1:5000}")
+class DbAdapter(restTemplateBuilder: RestTemplateBuilder) {
+    @Value("\${dbClient.base.url:http://127.0.0.1:4000}")
     private lateinit var baseUrl: String
+    private val dbClient = restTemplateBuilder.build()
     private val LOG: Logger = LoggerFactory.getLogger(DbAdapter::class.java)
     private val INTEGRATION_ERROR_TEXT: String = "DbAdapter Integration ERROR"
 
 
-    fun getEvent(longreadId: String, chapterId: String, blockContentId: String): String {
-        LOG.info("getEvent: $longreadId, $chapterId, $blockContentId ; dbClientUrl = $baseUrl")
+// event
+    fun getLongreadEvents(longreadId: Long): String {
+        val relativeUrl = "/longread/blockcontent/all"
+        LOG.info("getLongreadEvents: $longreadId ; dbClientUrl = $baseUrl, relativeUrl = $relativeUrl")
+        val rq = UniversalTimelineRq(null, null, null, null, null, longreadId)
 
-        return sendRequest()
+        return sendRequest(baseUrl + relativeUrl, rq, String::class.java)
+    }
+    fun getEvent(blockContentId: Long): String {
+        val relativeUrl = "/longread/blockcontent/event"
+        LOG.info("getEvent: $blockContentId ; dbClientUrl = $baseUrl, relativeUrl = $relativeUrl")
+        val rq = UniversalTimelineRq(blockContentId, null, null, null, null, null)
+
+        return sendRequest(baseUrl + relativeUrl, rq, String::class.java)
     }
 
-    fun getLongreadMap(longreadId: String): String {
-        LOG.info("getLongreadMap: $longreadId ; dbClientUrl = $baseUrl")
-
-        return sendRequest()
+    fun updateEventOfBlockContent(blockContentId: Long, xCoordinate: Long,  yCoordinate: Long, time: Long, floatingText: String) {
+        val relativeUrl = "/longread/blockcontent/event/edit"
+        LOG.info("updateEventOfBlockContent: $blockContentId ; dbClientUrl = $baseUrl, relativeUrl = $relativeUrl")
+        val rq = UniversalTimelineRq(blockContentId, xCoordinate, yCoordinate, time, floatingText, null)
+        sendRequest(baseUrl + relativeUrl, rq, String::class.java)
     }
 
-    fun getLongreadEvents(chapterId: String): String  {
-        LOG.info("getLongreadEvents: $chapterId ; dbClientUrl = $baseUrl")
-
-        return sendRequest()
+    fun deleteEventOfBlockContent(blockContentId: Long) {
+        val relativeUrl = "/longread/blockcontent/event/delete"
+        LOG.info("deleteEventOfBlockContent: $blockContentId ; dbClientUrl = $baseUrl, relativeUrl = $relativeUrl")
+        val rq = UniversalTimelineRq(blockContentId, null, null, null, null, null)
+        sendRequest(baseUrl + relativeUrl, rq, String::class.java)
     }
 
+// map
+    fun getMapImgUrl(longreadId: Long): String {
+        val relativeUrl = "/longread/map"
+        LOG.info("getMapImgUrl: $longreadId ; dbClientUrl = $baseUrl, relativeUrl = $relativeUrl")
+        val rq = UniversalTimelineRq(null, null, null, null, null, longreadId)
+
+        return sendRequest(baseUrl + relativeUrl, rq, String::class.java)
+    }
+
+// timeline
+    fun getTimelineImgUrl(longreadId: Long): String {
+        val relativeUrl = "/longread/timeline"
+        LOG.info("getTimelineImgUrl: $longreadId ; dbClientUrl = $baseUrl, relativeUrl = $relativeUrl")
+        val rq = UniversalTimelineRq(null, null, null, null, null, longreadId)
+
+        return sendRequest(baseUrl + relativeUrl, rq, String::class.java)
+    }
+
+//    fun getLongreadEvents(chapterId: String): String  {
+//        LOG.info("getLongreadEvents: $chapterId ; dbClientUrl = $baseUrl")
+//
+//
+//        return sendRequest()
+//    }
 
 
 
-    private inline fun <reified T> sendRequest(url: String, request: Any?, responseType: T) : T {
-        requireNotNull(responseType)
-        LOG.info("Sending request on $url")
-        val response = dbClient.postForEntity(url, request, responseType!!::class.java)
+
+    private inline fun <reified T> sendRequest(url: String, request: Any?, responseType: Class<T>) : T {
+        LOG.info("Sending request $request on $url")
+        val response = dbClient.postForEntity(url, request, responseType)
         if (response.statusCode.is2xxSuccessful) {
             requireNotNull(response.body)
             LOG.info("Got response: ${response.body}")
